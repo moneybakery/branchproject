909c7328d3c7e60bedd09acd4ed85608321e690a
moneybakery <30450873+moneybakery@users.noreply.github.com>
2017-08-02 12:16:44 +0300

    Changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("AUD");
+    case mBTC: return QString("mAUD");
+    case uBTC: return QString::fromUtf8("μAUD");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("Branch");
+    case mBTC: return QString("Milli-Branch (1 / 1,000)");
+    case uBTC: return QString("Micro-Branch (1 / 1,000,000)");
     default: return QString("???");
     }
 }
